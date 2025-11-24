b385b73349fd038a85f3910c0d0014c1c676fa47
sofarun313131 <sofa.run31.31.31@gmail.com>
2025-11-24 13:29:09 +0300

    header.h

--- header.h
+++ header.h
@@ -14,5 +14,10 @@ struct Group
     std::string Id;
     std::vector<Student> Students;
 };
+void SortByName(std::vector<Student>&);
+void SortByRating(std::vector<Student>&);
+size_t CountTwoness(const std::vector<Student>&);
+size_t CountExcellent(const std::vector<Student>&);
+std::vector<Student> VectorMathExcellent(const std::vector<Student>&);
 std::vector<std::string> GroupsId(const std::vector<Student>&);
 std::vector<Group> Groups(const std::vector<Student>&);
